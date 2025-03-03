965f20958e386f5c6a981b3d414fb1b56659444d
Rahulkumar Gupta <159034627+grahulkumar@users.noreply.github.com>
2025-03-03 20:35:29 +0530

    add payment_id

--- apps/landlord/models.py
+++ apps/landlord/models.py
@@ -26,6 +26,7 @@ class Rentdetails(models.Model):
     total_days=models.IntegerField(default=1)
     total_price=models.DecimalField( max_digits=10, decimal_places=2,default=1)
     pay_status=models.CharField(max_length=50,default="pending")
+    payment_id=models.CharField(max_length=200,default="sampleid")
     u=models.ForeignKey(CustomUser, on_delete=models.CASCADE, default=1)
     p=models.ForeignKey(HomeDetails, on_delete=models.CASCADE, default=1)
 
